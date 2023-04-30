f17b79636a70ead3e571af690e255602ae833e9e
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2023-05-01 00:48:06 +0100

    item to pick up

---
+++ GameOfLife/Assets/Scripts/Berry.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Berry : MonoBehaviour
+{
+    public GameObject self;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Among Us");
+        if (other.tag == "Player")
+        {
+            other.gameObject.tag = "Berry";
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Destroy(self);
+    }
+}
